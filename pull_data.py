52ab47680fa9baa784145700cfb624650a556028
trimnac <timothy.rimnac@gmail.com>
2015-02-18 16:20:32 -0600

    Update pull_data.py
    Changed the progress output to be a single line with day number and percentage. Added header to the output.

--- pull_data.py
+++ pull_data.py
@@ -1,10 +1,13 @@
 import csv
 import urllib2
+import sys
 
 #set this to a really big number to reach all days between now and Jan 1 1948
-top_of_range = 25000
+top_of_range = 13
 
 all_data = []
+
+header_info = ["TimeCST","TemperatureF","Dew PointF","Humidity","Sea Level PressureIn","VisibilityMPH","Wind Direction","Wind SpeedMPH","Gust SpeedMPH","PrecipitationIn","Events","Conditions","WindDirDegrees","DateUTC"]
 
 for item in range(1, top_of_range):
 	downloaded_data  = urllib2.urlopen('http://www.wunderground.com/history/airport/KMDW/1948/1/' + str(item) + '/DailyHistory.html?req_city=Chicago&req_state=IL&req_statename=Illinois&reqdb.zip=60290&reqdb.magic=1&reqdb.wmo=99999&format=1')
@@ -20,14 +23,16 @@ for item in range(1, top_of_range):
 	#24271 represents the number of days from Jan 1 1948 to Jan 1 2014
 	#checking for greater than 24271 ensures that if a date in history has no data, it won't be skipped
 	if(all_data[-1][0] == "No daily or hourly history data available<br />" and item > 24271):
-		print("You've reached the end!")
+		sys.stdout.write("You've reached the end!")
 		break
 	#todo: remove header and blank row from the top of each page
-	print("done with day: {}".format(item))
+	percent = float(item) / top_of_range
+	sys.stdout.write("\rDone with day: {0} {1}%".format(item, round((percent) * 100, 2)))
+	sys.stdout.flush()
 
+all_data.insert(0, header_info)
 
-
-print("the total number of rows is {}.".format(len(all_data)))
+print("\nthe total number of rows is {}.".format(len(all_data)))
 
 with open("chicago_weather_data.csv", "w+") as f:
     writer = csv.writer(f)
