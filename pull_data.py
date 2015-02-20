8aedc937b4ee101f4480c5ce7b04efde41dd418a
trimnac <timothy.rimnac@gmail.com>
2015-02-19 19:31:15 -0600

    Update pull_data.py
    Fixed an error when the number of days is greater than the end of data. Now properly stopping without error and finishing the program early. Also removed <br /> from the end of the last item of each row.

--- pull_data.py
+++ pull_data.py
@@ -3,29 +3,38 @@ import urllib2
 import sys
 
 #set this to a really big number to reach all days between now and Jan 1 1948
-top_of_range = 13
+top_of_range = 25000
+start_year = 2015
 
 all_data = []
 
 header_info = ["TimeCST","TemperatureF","Dew PointF","Humidity","Sea Level PressureIn","VisibilityMPH","Wind Direction","Wind SpeedMPH","Gust SpeedMPH","PrecipitationIn","Events","Conditions","WindDirDegrees","DateUTC"]
 
 for item in range(1, top_of_range):
-	downloaded_data  = urllib2.urlopen('http://www.wunderground.com/history/airport/KMDW/1948/1/' + str(item) + '/DailyHistory.html?req_city=Chicago&req_state=IL&req_statename=Illinois&reqdb.zip=60290&reqdb.magic=1&reqdb.wmo=99999&format=1')
+	downloaded_data  = urllib2.urlopen('http://www.wunderground.com/history/airport/KMDW/' + str(start_year) + '/1/' + str(item) + '/DailyHistory.html?req_city=Chicago&req_state=IL&req_statename=Illinois&reqdb.zip=60290&reqdb.magic=1&reqdb.wmo=99999&format=1')
 	csv_data = list(csv.reader(downloaded_data))
+
 	#removes blank rows
 	if(csv_data[0] == []):
 		del csv_data[0]
+	
 	#removes extra headers from each file
 	if(csv_data[0][0] == "TimeCST"):
 		del csv_data[0]
-	all_data.extend(csv_data)
+
 	#this will end it automatically once the top bound is reached
-	#24271 represents the number of days from Jan 1 1948 to Jan 1 2014
-	#checking for greater than 24271 ensures that if a date in history has no data, it won't be skipped
-	if(all_data[-1][0] == "No daily or hourly history data available<br />" and item > 24271):
+	if(csv_data[1][0] == "No daily or hourly history data available<br />" or csv_data[0][0] == "Time"):
 		sys.stdout.write("You've reached the end!")
 		break
-	#todo: remove header and blank row from the top of each page
+
+	for line in csv_data:
+		for char in '<br/>':
+			line[13] = line[13].replace(char, '')
+		line[13] = line[13].strip()
+
+	all_data.extend(csv_data)
+
+	#Console progress output
 	percent = float(item) / top_of_range
 	sys.stdout.write("\rDone with day: {0} {1}%".format(item, round((percent) * 100, 2)))
 	sys.stdout.flush()
