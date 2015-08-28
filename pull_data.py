6e9f806d0bd68be69e16ed6762ee6609be0c2a68
trimnac <timothy.rimnac@gmail.com>
2015-08-27 20:16:20 -0500

    Update pull_data.py
    Bug fix that crashes script if the page is blank

--- pull_data.py
+++ pull_data.py
@@ -1,38 +1,44 @@
 import csv
-import urllib2
+import urllib.request
 import sys
+import io
 
 #set this to a really big number to reach all days between now and Jan 1 1948
-top_of_range = 6150
-start_year = 2001
+top_of_range = 26000
+start_year = 1948
 
 all_data = []
 
 header_info = ["TimeCST","TemperatureF","Dew PointF","Humidity","Sea Level PressureIn","VisibilityMPH","Wind Direction","Wind SpeedMPH","Gust SpeedMPH","PrecipitationIn","Events","Conditions","WindDirDegrees","DateUTC"]
 
 for item in range(1, top_of_range):
-	downloaded_data  = urllib2.urlopen('http://www.wunderground.com/history/airport/KMDW/' + str(start_year) + '/1/' + str(item) + '/DailyHistory.html?req_city=Chicago&req_state=IL&req_statename=Illinois&reqdb.zip=60290&reqdb.magic=1&reqdb.wmo=99999&format=1')
-	csv_data = list(csv.reader(downloaded_data))
+	downloaded_data  = urllib.request.urlopen('http://www.wunderground.com/history/airport/KMDW/' + str(start_year) + '/1/' + str(item) + '/DailyHistory.html?req_city=Chicago&req_state=IL&req_statename=Illinois&reqdb.zip=60290&reqdb.magic=1&reqdb.wmo=99999&format=1')
+	#csv_data = list(csv.reader(io.TextIOWrapper(downloaded_data)))
+	csv_data = list(csv.reader(downloaded_data.read().decode('utf-8').splitlines()))
 
-	#removes blank rows
-	if(csv_data[0] == []):
-		del csv_data[0]
-	
-	#removes extra headers from each file
-	if(csv_data[0][0] == "TimeCST" or csv_data[0][0] == "TimeCDT"):
-		del csv_data[0]
+	#Checks to make sure the data isn't completely empty
+	if not csv_data:
 
-	#this will end it automatically once the top bound is reached
-	if(csv_data[1][0] == "No daily or hourly history data available<br />" or csv_data[0][0] == "Time"):
-		sys.stdout.write("You've reached the end!")
-		break
+		#removes blank rows
+		if(csv_data[0] == []):
+			del csv_data[0]
+		
+		#removes extra headers from each file
+		if(csv_data[0][0] == "TimeCST" or csv_data[0][0] == "TimeCDT"):
+			del csv_data[0]
 
-	for line in csv_data:
-		for char in '<br/>':
-			line[13] = line[13].replace(char, '')
-		line[13] = line[13].strip()
+		#this will end it automatically once the top bound is reached
+		if len(csv_data) > 1:
+			if(csv_data[1][0] == "No daily or hourly history data available<br />" or csv_data[0][0] == "Time"):
+				sys.stdout.write("You've reached the end!")
+				break
 
-	all_data.extend(csv_data)
+		for line in csv_data:
+			for char in '<br/>':
+				line[13] = line[13].replace(char, '')
+			line[13] = line[13].strip()
+
+		all_data.extend(csv_data)
 
 	#Console progress output
 	percent = float(item) / top_of_range
@@ -43,6 +49,6 @@ all_data.insert(0, header_info)
 
 print("\nthe total number of rows is {}.".format(len(all_data)))
 
-with open("chicago_weather_data.csv", "w+") as f:
+with open("all_chicago_weather_data.csv", "w+") as f:
     writer = csv.writer(f)
     writer.writerows(all_data)
